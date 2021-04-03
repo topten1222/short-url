1fde55eb129ffd9fa714f765a4098f1c791ffcb5
Montree Pheomthong <topten.1222@gmail.com>
2021-04-04 01:42:31 +0700

    remove index.php in config and update layout main

--- www/app/Views/layouts/main.php
+++ www/app/Views/layouts/main.php
@@ -7,9 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
 </head>
+<script>var base_url = '<?php echo base_url() ?>';</script>
 <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
 <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/style.css'); ?>">
-
+<script type="text/javascript" src="<?php echo base_url('assets/js/jquery-3.6.0.min.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/jquery.cookie.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/main.js'); ?>"></script>
 <body class="pace-done">
 <main id="content" role="main">
     <?php $this->renderSection('content'); ?>
