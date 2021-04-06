87be7e356bb7d35c6613347d20baccb1a72cc2eb
Montree Pheomthong <topten.1222@gmail.com>
2021-04-06 12:32:05 +0700

    fig bug alias url script append

--- www/public/assets/js/main.js
+++ www/public/assets/js/main.js
@@ -21,7 +21,7 @@ function send() {
                     }
                     var html = `<div class="col-8"><div class="url_options rounded-6"><p class="nazwa_link">Expired Date: ${data.expired_date}</p>`+
                         `<p class="data_link">${data.date}</p><p class="url_link"><a href="${data.url}" target="blank">${data.url}</a>`+
-                        `</p><p id="link" class="link_element"><a href="${base_url+data.alias}" class="short_url_l" target="blank">${base_url+data.alias}</a></p>`+
+                        `</p><p id="link" class="link_element"><a href="${base_url+'/'+data.alias}" class="short_url_l" target="blank">${base_url+'/'+data.alias}</a></p>`+
                         `<button class="btn-cutt i_s" onclick="copyToClipboard(this)" data-toggle="tooltip" data-html="true" data-placement="top" title="" data-original-title="Copy"><svg class="i_svg" viewBox="0 0 24 24"><use xlink:href="#i_copy"></use></svg></button>` +
                         `<div class="btn-cutt click_stats"><svg class="i_svg" viewBox="0 0 24 24"><use xlink:href="#i_chart"></use></svg><span><b class="badge">0</b>clicks</span></div></div></div>`+
                         `<div class="col-4"><div class="url_options rounded-6" style="text-align: center"><a href="https://chart.googleapis.com/chart?cht=qr&chl=${base_url+data.alias}&chs=200x200&choe=UTF-8" target="_blank">`+
